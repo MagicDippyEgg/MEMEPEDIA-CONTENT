694e2fa0eb63d5db60c0fbb45b3a7db73bdaaeac
MagicDippyEgg <65807390+MagicDippyEgg@users.noreply.github.com>
2025-04-28 17:24:09 +1000

    Update backup_to_drive.py

--- backup_to_drive.py
+++ backup_to_drive.py
@@ -2,12 +2,17 @@ import os
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 
+# Create a dictionary with the necessary data
+creds_info = {
+    'token': None,
+    'refresh_token': os.getenv('GOOGLE_DRIVE_REFRESH_TOKEN'),
+    'client_id': os.getenv('GOOGLE_DRIVE_CLIENT_ID'),
+    'client_secret': os.getenv('GOOGLE_DRIVE_CLIENT_SECRET'),
+    'scopes': ['https://www.googleapis.com/auth/drive']
+}
+
 # Authenticate using Google Drive API credentials
-creds = Credentials.from_authorized_user_info(
-    client_id=os.getenv('GOOGLE_DRIVE_CLIENT_ID'),
-    client_secret=os.getenv('GOOGLE_DRIVE_CLIENT_SECRET'),
-    refresh_token=os.getenv('GOOGLE_DRIVE_REFRESH_TOKEN')
-)
+creds = Credentials.from_authorized_user_info(creds_info)
 
 # Set up Google Drive service
 drive_service = build('drive', 'v3', credentials=creds)
